14de10282aaa1e49fd18ba9dcf3e2f256c19dea0
MaMorde <mamorde@yandex.ru>
2020-07-06 16:24:42 +0300

    regularize-appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -8,18 +12,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 import { LoginPageComponent } from './pages/login/login.page';
 import { SignupPageComponent } from './pages/signup/signup.page';
-import { ChatComponent } from './components/chat/chat.component';
 import { MainPageComponent } from './pages/main/main.page';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AuthGuard } from './guards/auth.guard';
+
 import { PickerModule } from '@ctrl/ngx-emoji-mart';
 
 @NgModule({
